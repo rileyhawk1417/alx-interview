b382b7cecf94067040a084c12f649a59e0c48dcb
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-01-22 14:52:05 +0200

    feat: complete logging assignment

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+Stat logger
+This module reads from stdin
+"""
+
+import re
+import sys
+import signal
+
+
+def sig_handler(sig, frame):
+    dump_metrics()
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, sig_handler)
+ip_regex = re.compile(
+    r'(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # IP Address
+    r' - \[(?P<date>.*?)\]'  # Date
+    r' "(?P<request>.*?)"'  # Request
+    r'(?P<status_code>\d{3})'  # Status Code
+    r'(?P<file_size>\d+)'  # File Size
+)
+
+stat_code_list = {200: 0,
+                  301: 0,
+                  400: 0,
+                  401: 0,
+                  403: 0,
+                  404: 0,
+                  405: 0,
+                  500: 0}
+counter = 0
+
+
+def dump_metrics():
+    """This function just prints metrics"""
+    print(f"File size: {total_size}")
+    for stat_code in sorted(stat_code_list.keys()):
+        occurence = stat_code_list[stat_code]
+        if occurence > 0:
+            print(f"{stat_code}: {occurence}")
+
+
+for line in sys.stdin:
+    ip_list_match = ip_regex.match(line)
+
+    if ip_list_match:
+        try:
+            status_code = int(ip_list_match.group("status_code"))
+            file_size = int(ip_list_match.group("file_size"))
+        except (ValueError, IndexError):
+            continue
+        total_size = 0
+        total_size += file_size
+        stat_code_list[status_code] += 1
+        counter += 1
+    if counter % 10 == 0:
+        dump_metrics()
---
+++ 0x03-log_parsing/README.md
@@ -0,0 +1,5 @@
+# Description
+
+This folder is to demonstrate python's scripting abilities.
+In order to run it use the following:
+`./rand_ip_generator.py | python3 0-stats.py`
