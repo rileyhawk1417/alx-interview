a6b9deea039ac26987a15edc2b196f62d9ca3e29
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-02-01 17:46:06 +0200

    chore: complete utf8 assignment

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def validUTF8(data):
+    """
+    Essentially its converting the data to
+    hexadecimal. Then from hexadecimal to bytes
+    after modifying it. From bytes its validated
+    against UTF-8
+    Args:
+        data: list of integers
+    Return:
+        boolean value. True if valid False if not
+    """
+    try:
+        bytes.fromhex(''.join(hex(bytez)[2:].zfill(4)
+                      for bytez in data)).decode('utf-8')
+        return True
+    except UnicodeDecodeError:
+        return False
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,5 @@
+# Description
+
+This folder has code about validating `utf-8` values.
+
+**TLDR;** UTF-8 is a standard in the web for validating and converting text.
