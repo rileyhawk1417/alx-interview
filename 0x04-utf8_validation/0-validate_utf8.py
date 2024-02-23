45089f509036e3fba8eaf9a1c3413032da0e2805
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-02-23 19:40:41 +0200

    refactor: redo file for UTF-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,18 +6,30 @@ Module that validates UTF-8
 
 def validUTF8(data):
     """
-    Essentially its converting the data to
-    hexadecimal. Then from hexadecimal to bytes
-    after modifying it. From bytes its validated
-    against UTF-8
-    Args:
-        data: list of integers
-    Return:
-        boolean value. True if valid False if not
+    Go through each byte in the dataset.
+    Then confirm if it follows the UTF-8 rule.
+    Returns:
+        bool: True if valid False if not
     """
-    try:
-        bytes.fromhex(''.join(hex(bytez)[2:].zfill(4)
-                      for bytez in data)).decode('utf-8')
-        return True
-    except UnicodeDecodeError:
-        return False
+    num_bytes = 0
+
+    for byte in data:
+        # Maksure the 8 least significant bits are used
+        byte = byte & 0xFF
+        if num_bytes == 0:
+            if byte >> 7 == 0b0:
+                continue
+            elif byte >> 5 == 0b110:
+                num_bytes = 1
+            elif byte >> 4 == 0b1110:
+                num_bytes = 2
+            elif byte >> 3 == 0b11110:
+                num_bytes = 3
+            else:
+                return False
+        else:
+            if byte >> 6 != 0b10:
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
