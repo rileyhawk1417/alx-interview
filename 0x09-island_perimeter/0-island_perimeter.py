7ecf05b22627b83d7d151a9d82927622be20e747
Sean Riley Hawkins <rileyhawk249@gmail.com>
2024-03-07 12:27:21 +0200

    feat: add additional function to check neighbouring cells

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -28,3 +28,30 @@ def island_perimeter_old(grid):
                     island -= 2
 
     return island
+
+
+def island_perimeter(grid):
+    """
+    island_perimeter - returns the perimeter of an island
+    @grid: 2D list of integers
+    Return: the perimeter of the island
+    """
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
+
+    for outer in range(rows):
+        for inner in range(cols):
+            if grid[outer][inner] == 1:
+                perimeter += 4  # Each land cell adds 4 to the perimeter
+
+                # Check neighboring cells
+                if outer > 0 and grid[outer - 1][inner] == 1:  # Up
+                    perimeter -= 1
+                if outer < rows - 1 and grid[outer + 1][inner] == 1:  # Down
+                    perimeter -= 1
+                if inner > 0 and grid[outer][inner - 1] == 1:  # Left
+                    perimeter -= 1
+                if inner < cols - 1 and grid[outer][inner + 1] == 1:  # Right
+                    perimeter -= 1
+
+    return perimeter
